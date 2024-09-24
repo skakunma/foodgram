fcd94f0048b1c413a0e51fde2dbd5f3b2885a749
Дмитрий Скакун <skakun2025@mail.ru>
2024-09-24 22:42:57 +0300

    last&

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -69,7 +69,7 @@ CORS_ALLOW_METHODS = [
 
 CSRF_TRUSTED_ORIGINS = ['http://skakunfoodgram.zapto.org', 'https://skakunfoodgram.zapto.org']
 
-CSRF_COOKIE_DOMAIN='skakunfoodgram.zapto.org'
+CSRF_COOKIE_DOMAIN = 'skakunfoodgram.zapto.org'
 
 CORS_ALLOW_HEADERS = [
     "content-type",
